723cf36a4bd44f61e4f306591d24cf7aaace79ff
Thomas Basham <Bashamtg@gmail.com>
2024-01-20 09:42:52 -0800

    initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <header>
+      <h1>Intro To react</h1>
+    </header>
+  );
+}
---
+++ src/components/Main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import "../styles/Main.css";
+
+export default function Main() {
+  return (
+    <main>
+      <p id="main-copy">
+        This is an intro to react Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Perferendis, ducimus doloremque. Ratione sint, non
+        eaque consequatur maiores consectetur pariatur, voluptates in aliquid
+        repellat recusandae hic quo, adipisci sapiente totam rerum! Perferendis
+        expedita neque qui asperiores quisquam cum libero laborum
+        necessitatibus, harum recusandae facere ratione dolores possimus quod
+        quibusdam consequuntur vero dolor. Veniam dolores blanditiis ullam est
+        quo aliquid, fugiat delectus?
+      </p>
+    </main>
+  );
+}

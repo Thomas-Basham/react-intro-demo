9e5dd094a83fb86ff0637a3ef9e0f43e6fad6f62
Thomas Basham <Bashamtg@gmail.com>
2024-01-22 17:13:00 -0800

    added counter to main element

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,9 +2,37 @@ import React from "react";
 import "../styles/Main.css";
 
 export default function Main() {
+  const [count, setCount] = React.useState(0);
+
+  const fruit = "blueberry";
+  const age = 26;
+  const someFruits = ["blackberry", "apple", "banana"]
+
+
+  function handleButtonClick(){
+    setCount(count + 1)
+  }
+
   return (
     <main>
-      <p id="main-copy">
+
+      {fruit}
+
+      <br />
+
+      {age >= 21 ? "Beer" : "Juice"}
+
+      <br />
+
+      {fruit == 'blueberry' && "it's blue berry time!"}
+
+      {someFruits.map((fruit, index) => <li key={index}>{fruit}</li>)}
+
+      
+      <p>Count is: {count}</p>
+      <button onClick={handleButtonClick}>Click Me</button>
+
+      <p className='' id="main-copy">
         This is an intro to react Lorem ipsum dolor sit amet consectetur
         adipisicing elit. Perferendis, ducimus doloremque. Ratione sint, non
         eaque consequatur maiores consectetur pariatur, voluptates in aliquid
